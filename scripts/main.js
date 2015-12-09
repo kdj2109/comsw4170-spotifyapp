73165c854a1c087688db5e372617d397a2211c6d
Daria Jung <djj2115cu@gmail.com>
2015-12-08 21:38:23 -0500

    jsonp reviews -_-

--- scripts/main.js
+++ scripts/main.js
@@ -3,27 +3,42 @@ var spotifyApp = angular.module('spotifyApp', ['ngMaterial']);
 // HIDE THIS SOMEHOW
 var API_KEY = '';
 
-spotifyApp.controller('SearchController', function($scope) {
-  $scope.testing = [{
-    'meow': 'kitkat'
-  }];
+
+// takes an obj literal and converts it to a query string
+var serialize = function(obj) {
+  var str = [];
+  for(var p in obj)
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  return str.join("&");
+}
+
+spotifyApp.controller('SearchController', function($scope, $http) {
+
+  $scope.myArtists = [];
 
   $scope.form = {
     artist: ""
   };
 
   // reviews endpoint
-  $scope.getReviews = function() {
-    var req = {
-      method: 'POST',
-      url: 'http://example.com',
-      headers: {
-       'Content-Type': undefined
-      },
-      data: { test: 'test' }
-    }
+  $scope.getReviews = function(artistName) {
 
-    $http(req).then(function(){...}, function(){...});
+    var req = { 'name': artistName, 'api_key': API_KEY, 'format': 'jsonp' };
+
+    var url = 'http://developer.echonest.com/api/v4/artist/reviews?callback=JSON_CALLBACK&' + serialize(req);
+
+    console.log(url);
+
+    $http.jsonp(url).success(function(data) { 
+      console.log('success');
+      console.log(data);
+    }).
+    error(function(data) {
+      console.log(data);
+      console.log('errorrrrrrr')
+    });
   }
 
   // news endpoint
