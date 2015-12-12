6bf073cce9f4bf70afbfc0950120bf4c6224d23b
Lauren O'Connor <leo2118@columbia.edu>
2015-12-12 13:21:04 -0500

    added profile picture

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,12 @@ var API_KEY = 'NGB9ACOOVZV9AOTEZ';
 
 spotifyApp.controller('ArtistController', function($scope, $http, $sce) {
 
+  //initial variables
   $scope.songs = [];
   $scope.trackset = "";
   $scope.myArtists = ["Kendrick Lamar", "Fetty Wap","Beyonce", "Nicki Minaj", "Justin Bieber"];
+  $scope.newsfeed = false;
+
 
   $scope.form = {
     artist: ""
@@ -17,30 +20,39 @@ spotifyApp.controller('ArtistController', function($scope, $http, $sce) {
   $scope.switchArtist = function(artistName) {
 
     if(artistName=="newsfeed"){
-        //populate newsfeed instead
+        var frame="<span></span>";
+        $scope.iframe = $sce.trustAsHtml(frame);
+        $scope.newsfeed=false;
 
     }else{
       var url = 'https://api.spotify.com/v1/search?q='+artistName+'&type=artist&limit=1';
 
       //get artist ID from name
       $http.get(url).success(function(data) { 
-        var artistID = data.artists.items[0].id;
+        $scope.newsfeed=true;
+        $scope.artistData = data.artists;
+        $scope.artistPicture = data.artists.items[0].images[0].url;
+        $scope.artistName = data.artists.items[0].name;
+        var artistID = data.artists.items[0].id
+
 
         //get top tracks from artist ID
         url = 'https://api.spotify.com/v1/artists/'+artistID+'/top-tracks?country=US';
         console.log(url);
         $http.get(url).success(function(data){
             $scope.trackset="";
+            $scope.artistData="";
             $scope.songs = data.tracks;
             
             //create iframe spotify list
-            for(var i=0; i<9;i++){
+            for(var i=0; i<5;i++){
               var id = $scope.songs[i].id + ",";
               $scope.trackset = $scope.trackset.concat(id);
             } 
             var frame="<iframe src='https://embed.spotify.com/?uri=spotify:trackset:title:"+
-            $scope.trackset+"' frameborder='0' width='640' height='600'></iframe>";
+            $scope.trackset+"' frameborder='0' width='320' height='315' style='display:inline'></iframe>";
             $scope.iframe = $sce.trustAsHtml(frame);
+
         }).error(function(data){
           console.log('tracks not found');
         })
@@ -51,10 +63,9 @@ spotifyApp.controller('ArtistController', function($scope, $http, $sce) {
         console.log('artist not found')
       });
     }
+
+
   }
-
-
-
 
 
 }).config(function($mdThemingProvider) {
