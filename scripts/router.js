072c09dee6ca118c8b7a7b156b0591b8bbf59eea
Kyle Jackson <kdj2109@columbia.edu>
2015-12-12 17:06:24 -0500

    header was covering everything

--- scripts/router.js
+++ scripts/router.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-var spotify_app = angular.module('spotify_app', ['ngRoute', 'ngMaterial', 'spotifyControllers',]);
-=======
 var spotify_app = angular.module('spotify_app', ['ngRoute', 'spotifyControllers', 'ngMaterial', 'artistPage']);
->>>>>>> 4e3eeb6558e84645e5e400e02c5133826e859c4d
 
 spotify_app.config(['$routeProvider', '$mdThemingProvider',
   function($routeProvider) {
